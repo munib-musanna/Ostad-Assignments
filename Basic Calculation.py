40e4ff3c4e5785bb302415a93ce4a485558ead8f
Munib Musanna <munibmusanna2001@gmail.com>
2026-07-04 00:13:51 +0600

    Add files via upload

---
+++ Basic Calculation.py
@@ -0,0 +1,17 @@
+# Write a program that takes two numbers 
+# and an operator (+, -, *, /) and performs the calculation.
+
+a=int(input("Enter the number: "))
+b=int(input("Enter the number: "))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print("Addition: ",c)
+print("Subtraction: ",d)
+print("Multiplication: ",e)
+print("Division: ",f)
+
+#Answers will depend on whether a>b,a<b or a=b
